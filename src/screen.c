9a600a3722d4910a6a610bf56cd08fce80f5984c
waporwave <thecookielegends@gmail.com>
2024-12-09 20:59:31 -0600

    Update screen.c

--- src/screen.c
+++ src/screen.c
@@ -87,7 +87,9 @@ void InitializeSDL() {
     );
 
     for (int i = 0; i<400; i++) {
-        TextmodeGrid[i] = (TextmodeGrid){0,0,4};
+        TextmodeGrid[i].character = 0;
+        TextmodeGrid[i].bg_color = 0;
+        TextmodeGrid[i].fg_color = 0;
     }
 }
 
